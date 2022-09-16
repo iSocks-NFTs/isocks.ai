17c526166c560ca84cb2611d94a9990704e7851f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-16 09:46:13 +0100

    added built components to index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-import Button from 'react-bootstrap/Button';
+import Head from 'next/head';
+import Tutorial from '../components/ui/Tutorials';
+import Footer from '../components/Footer';
+import Roadmap from '../components/Roadmap';
 
 export default function Home() {
   return (
     <>
-      <Button>Hello</Button>
+      <Head>
+        <title>iSock | Homepage</title>
+      </Head>
+      <Tutorial />
+      <Roadmap />
+      <Footer />
     </>
   )
 }
