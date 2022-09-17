94f0431e5362ca87e331b6e2eeb3311ca49058bb
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-17 01:32:38 +0100

    created coming soon componentn

---
+++ components/ComingSoon/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ComingSoonGrid, Text, Container, Heading, Stage } from "./comingSoon";
+import { Row, Col } from "react-bootstrap";
+
+const ComingSoon = () => {
+  return (
+    <Container>
+      <Heading>
+        Wear it, trade it, and never worry about a missing socks. <br />iSocks is a
+        physical product with blockchain records of the owner.
+      </Heading>
+      <ComingSoonGrid>
+        <Stage src="/img/logo/isock.svg" alt="iSock SVG" />
+        <Text>Coming Soon...</Text>
+        <Stage src="/img/logo/binance.svg" alt="Binance SVG" />
+      </ComingSoonGrid>
+    </Container>
+  );
+};
+
+export default ComingSoon;
