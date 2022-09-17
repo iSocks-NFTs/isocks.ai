435e1fbd90c4eb1238e767567b9e6edf68b2be75
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-17 01:34:06 +0100

    set up home page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,39 @@
-import Head from 'next/head';
-import Tutorial from '../components/ui/Tutorials';
-import Footer from '../components/Footer';
-import Roadmap from '../components/Roadmap';
+import React, { useState } from "react";
+import Head from "next/head";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Tutorial from "../components/ui/Tutorials";
+import Footer from "../components/Footer";
+import Roadmap from "../components/Roadmap";
+import Subscribe from "../components/Subscribe";
+
+import ComingSoon from "../components/ComingSoon";
+
 
 export default function Home() {
+  const [ready, setReady] = useState(false);
+
   return (
     <>
       <Head>
         <title>iSock | Homepage</title>
       </Head>
-      <Tutorial />
-      <Roadmap />
-      <Footer />
+      {ready ? (
+        <>
+          <Navbar />
+          <Tutorial />
+          <Roadmap />
+          <Subscribe />
+          <Footer />
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <ComingSoon />
+          <Subscribe />
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
