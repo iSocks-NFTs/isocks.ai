347d7a8e81f304e30eccf73a6f43df3b06388da9
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-18 09:37:31 +0100

    added modal to join waiting list page

--- pages/join-whitelist/index.js
+++ pages/join-whitelist/index.js
@@ -1,11 +1,25 @@
-import Layout from '../../layouts/normal_layout';
+import React from "react";
+import Layout from "../../layouts/normal_layout";
+import JoinWhiteList from "../../components/JoinWhiteListForm";
+import Modal from "../../components/Modal";
+import { GlobalContext } from "../../context/globalContext";
 
-const JoinWaitList = () =>{
-    return(
-        <Layout>
-            
-        </Layout>
-    )
-}
+const JoinWaitList = () => {
+
+    const {comingSoonModal} = React.useContext(GlobalContext);
+
+  return (
+    <Layout>
+      <JoinWhiteList />
+      {comingSoonModal ? (
+        <>
+          <Modal />
+        </>
+      ) : (
+        <></>
+      )}
+    </Layout>
+  );
+};
 
 export default JoinWaitList;
