715f8350882f76ccfd264471623ba5e9cdfcc96a
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-18 09:36:46 +0100

    added global context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import React from 'react'
+import GlobalContextProvider from '../context/globalContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GlobalContextProvider>
+      <Component {...pageProps} />
+    </GlobalContextProvider>
+  )
 }
 
 export default MyApp
