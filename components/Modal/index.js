b1a2f760efa3296a9e778cb215fd02c1af41252f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-20 19:28:50 +0100

    added framer motion

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,17 +1,37 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../../context/globalContext";
-import { BackGround,ISockLogo,ModalContainer } from "./Modalstyle";
+import { BackGround, ISockLogo, ModalContainer } from "./Modalstyle";
+import { motion } from "framer-motion";
 
-const Modal = () =>{
-    const {comingSoonModal,setComingSoonModal} = useContext(GlobalContext);
-    return(
-        <BackGround onClick={() => setComingSoonModal(!comingSoonModal)}>
-            <ModalContainer>
-                <ISockLogo src="/img/logo/logo.png" alt="iSock Logo" />
-                Coming Soon!
-            </ModalContainer>
-        </BackGround>
-    )
-}
+const Modal = () => {
+  const { comingSoonModal, setComingSoonModal } = useContext(GlobalContext);
+
+  const dropIn = {
+    hidden: {},
+    visible: {},
+    exit: {},
+  };
+
+  return (
+    <BackGround
+      as={motion.div}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      onClick={() => setComingSoonModal(!comingSoonModal)}
+    >
+      <ModalContainer
+        as={motion.div}
+        onClick={(e) => e.stopPropagation()}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <ISockLogo src="/img/logo/logo.png" alt="iSock Logo" />
+        Coming Soon!
+      </ModalContainer>
+    </BackGround>
+  );
+};
 
 export default Modal;
