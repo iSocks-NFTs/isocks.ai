2544d4d48d37d9da47d0a7e9f902cf9de1a31fbc
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-21 14:49:43 +0100

    added links and icons

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import {GlobalContext} from '../../context/globalContext'
 import { Container, Image,Grid, ImgCaption,Heading,ContactUs,SocialContainer,Socials,Information,Brand } from "./footerStyles";
 import Link from 'next/link';
+import { FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () =>{
     const {setComingSoonModal} = React.useContext(GlobalContext);
@@ -16,18 +17,18 @@ const Footer = () =>{
                     <Socials>
                         <Heading>Socials</Heading>
                         <SocialContainer>
-                            <span onClick={() => setComingSoonModal(true)}>
+                            <Link href="https://www.facebook.com/100084982976478/posts/pfbid02n18vDRwtfzP8xeg1cuQeV4PHJHHZguSXydhU3vw1yzCS2x4dFC5ENo26eZB4qt4Ql/">
+                                <FaFacebookSquare size={25} color="#fff" />
+                            </Link>
+                            <Link href="https://www.tiktok.com/@isocksnft?_t=8VmXPtv8jH4&_r=1">
                                 <Image src="/img/logo/tiktok.svg" alt="icon"/>
-                            </span>
-                            <span onClick={() => setComingSoonModal(true)}>
-                                <Image src="/img/logo/telegram.svg" alt="icon"/>
-                            </span>
+                            </Link>
                             <Link href="https://mobile.twitter.com/isocksNft">
                                 <Image src="/img/logo/twitter.svg" alt="icon"/>
                             </Link>
-                            <span onClick={() => setComingSoonModal(true)}>
+                            <Link href="https://www.instagram.com/isocksnft/">
                                 <Image src="/img/logo/ig.svg" alt="icon"/>
-                            </span>
+                            </Link>
                             <Link href="https://discord.gg/nbrsZY9z59">
                                 <Image src="/img/logo/discord.svg" alt="icon"/>
                             </Link>
