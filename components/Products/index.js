c2f16eadf88143a184ec6fb3fc7fb5156fb60b74
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-09-26 20:35:02 +0100

    created new updates

---
+++ components/Products/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ProductSlide } from "./style";
+
+const ProductSlide = ({productList}) => {
+  const [active, setActive] = React.useState([]);
+  return(
+    <>
+        
+    </>
+  );
+};
+
+export default ProductSlide;
---
+++ components/Products/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 100%;
+`
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    column-gap:0.8rem;
+`
+
+export const OutLineButton = styled.button`
+
+`
+
+export const FilledButton = styled.button`
+
+
+`
+
+export const ProductCard = styled.div`
+
+`
+
+export const ProductDescription = styled.span``
