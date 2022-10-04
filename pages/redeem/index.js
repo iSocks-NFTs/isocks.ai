a38014ee080a931c2cdaafa567c541059b01c395
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-04 17:54:40 +0100

    created redeem page

--- pages/redeem/index.js
+++ pages/redeem/index.js
@@ -1,7 +1,18 @@
 import { Row, Col } from "react-bootstrap";
+import Layout from '../../layouts/normal_layout';
+import Head from "next/head";
+import RedeemedComponent from "../../components/Redeem";
+
 
 const Redeem = () => {
-  return <></>;
+  return (
+    <Layout>
+      <Head>
+        <title>iSocks | Redeem</title>
+      </Head>
+      <RedeemedComponent />
+    </Layout>
+  );
 };
 
 export default Redeem;
