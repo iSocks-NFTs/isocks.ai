d986e73d7d50b47a21926e562d616e1bf77b7ea4
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-07 09:16:44 +0100

    added preorder component

--- pages/preorder/index.js
+++ pages/preorder/index.js
@@ -1,8 +1,15 @@
+import Head from "next/head"
+import PreOrderComponent from "../../components/PreOrder"
+import Layout from '../../layouts/normal_layout';
+
 const PreOrder = () =>{
     return(
-        <>
-        
-        </>
+        <Layout>
+            <Head>
+                <title>iSocks | Pre Order</title>
+            </Head>
+            <PreOrderComponent />
+        </Layout>
     )
 }
 
