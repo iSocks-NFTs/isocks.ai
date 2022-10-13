2c5bfa34a5923153f65c740aefdc8e072b3cd0e6
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-13 09:06:10 +0100

    created success modal

---
+++ components/Modal/success/index.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../../../context/globalContext";
+import { BackGround, Heading, ISockLogo, ModalContainer, P } from "./Modalstyle";
+import { motion } from "framer-motion";
+
+const SuccessModal = () => {
+  const { successModal,setSuccessModal } = useContext(GlobalContext);
+
+  return (
+    <BackGround
+      as={motion.div}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      onClick={() => setSuccessModal(!successModal)}
+    >
+      <ModalContainer
+        as={motion.div}
+        onClick={(e) => e.stopPropagation()}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <ISockLogo src="/img/icons/tick-circle.svg" alt="Tick Icon" />
+        <Heading>Successful</Heading>
+        <P>The action has been successful. thank you.</P>
+      </ModalContainer>
+    </BackGround>
+  );
+};
+
+export default SuccessModal;
