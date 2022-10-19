a7e29d867777ca456f161126d0e56c1a26634668
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-19 14:46:01 +0100

    added auth context to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import '../styles/globals.css'
-import React from 'react'
-import GlobalContextProvider from '../context/globalContext'
+import "../styles/globals.css";
+import React from "react";
+import GlobalContextProvider from "../context/globalContext";
+import AuthContextProvider from "../context/authContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GlobalContextProvider>
-      <Component {...pageProps} />
+      <AuthContextProvider>
+        <Component {...pageProps} />
+      </AuthContextProvider>
     </GlobalContextProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
