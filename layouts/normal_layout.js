b2e3241dfbd3ccfad8d1afd0012b2b99ebfbf31f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-21 03:10:41 +0100

    added icon to normal layout

--- layouts/normal_layout.js
+++ layouts/normal_layout.js
@@ -1,14 +1,15 @@
-import React, { useContext } from "react";
-import { GlobalContext } from "../context/globalContext"
+import React from "react";
+import Head from "next/head";
 import NavBar from "../components/Navbar/";
 import Subscribe from '../components/Subscribe'
 import Footer from "../components/Footer";
 
 export default function Layout({ children }) {
-
-  
   return (
     <>
+      <Head>
+        <link rel="icon" href="/img/icons/logo.svg" />
+      </Head>
       <NavBar />
       {children}
       <Subscribe />
