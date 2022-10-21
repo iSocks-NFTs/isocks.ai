4619c3905ba6f345a31dadbc9552e8b8acf2dfe0
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-21 03:02:39 +0100

    created styling for dashboard

---
+++ components/Dashboard/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 5px 1rem;
+`
+
+export const Heading = styled.h3`
+    font-size:28px;
+    text-align: center;
+    font-weight: ${({fontWeight})  => fontWeight ? fontWeight : ''};
+`
+
+export const Span = styled.span`
+    font-weight: 400;
+`
