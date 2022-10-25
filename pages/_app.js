8d3f214b1843ba2123e738cc4b416c76e1c591fc
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-25 23:53:00 +0100

    added transaction context

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import React from "react";
 import GlobalContextProvider from "../context/globalContext";
 import AuthContextProvider from "../context/authContext";
+import TransactionContextProvider from "../context/transactionContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GlobalContextProvider>
       <AuthContextProvider>
-        <Component {...pageProps} />
+        <TransactionContextProvider>
+          <Component {...pageProps} />
+        </TransactionContextProvider>
       </AuthContextProvider>
     </GlobalContextProvider>
   );
