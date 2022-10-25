ea7b0f23796f102db5f9a7323f4936699616f792
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-26 00:02:05 +0100

    tweaked transaction context

--- context/transactionContext.js
+++ context/transactionContext.js
@@ -3,35 +3,45 @@ import React from "react";
 export const TransactionContext = React.createContext({
   userId: "",
   transactionId: "",
-  otp:"",
-  setTransactionData: () => {},
+  storeUserID:(userId) => {},
+  storeTransactionID:(transactionId) => {}
 });
 
 const TransactionContextProvider = ({ children }) => {
   const [transactionData, setTransactionData] = React.useState({
     userId: "",
     transactionId: "",
-    otp:''
   });
 
   React.useEffect(() => {
     const storedTransactionInformation =
       window.localStorage.getItem("transactionData");
     if (!storedTransactionInformation) {
-        setTransactionData({...transactionData,userId:'',
-        transactionId:'',
-        otp:''
-    })
+      setTransactionData({
+        ...transactionData,
+        userId: "",
+        transactionId: "",
+      });
     }
   }, []);
+
+  const storeUserID = (userId) =>{
+    setTransactionData({...transactionData,userId});
+    window.localStorage.setItem('userId',userId);
+  }
+
+  const storeTransactionID = (transactionId) =>{
+    setTransactionData({...transactionData,transactionId});
+    window.localStorage.setItem('transactionId',transactionId)
+  }
 
   return (
     <TransactionContext.Provider
       value={{
         userId: transactionData.userId,
         transactionId: transactionData.transactionId,
-        otp:transactionData.otp,
-        setTransactionData,
+        storeUserID,
+        storeTransactionID
       }}
     >
       {children}
