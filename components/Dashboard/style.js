bddb6a564ce0310ae85d7907f3f91324c8b5fab9
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-26 16:27:57 +0100

    styling for transaction feed block

--- components/Dashboard/style.js
+++ components/Dashboard/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    padding: 5px 1rem;
+    padding: 1rem;
 `
 
 export const Heading = styled.h3`
@@ -10,6 +10,36 @@ export const Heading = styled.h3`
     font-weight: ${({fontWeight})  => fontWeight ? fontWeight : ''};
 `
 
+export const TableContainer = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap:1rem;
+    padding:1rem;
+    height:50vh;
+    overflow-y: scroll;
+`
+
 export const Span = styled.span`
     font-weight: 400;
 `
+
+export const Block = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: var(--primary-brand);
+    color:#fff;
+    width:fit-content;
+    padding:1rem;
+    transition:0.8s;
+    border-radius:8px;
+    :hover{
+        cursor:pointer;
+        transform: scale(1.05);
+    }
+    @media screen and (max-width:520px){
+        font-size:10px;
+    }
+`
+
+export const TransactionData = styled.span``
