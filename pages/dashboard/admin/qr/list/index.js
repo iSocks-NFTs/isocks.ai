99e8e312fba4e90491fafa898ace22ed25231ba6
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-27 17:06:30 +0100

    imported components

--- pages/dashboard/admin/qr/list/index.js
+++ pages/dashboard/admin/qr/list/index.js
@@ -1,4 +1,10 @@
+import Head from "next/head";
 import QRCodeImage from "../../../../../components/QR/Module";
+import Layout from "../../../../../layouts/admin_layout";
+import { QRContainer,Container,CodeBox,CodeLabel, LinkHref,LinkText,Heading } from "../../../../../components/QR/style";
+import { Button, ButtonContainer } from "../../../../../components/Form";
+import Router from "next/router";
+import {Row,Col} from 'react-bootstrap'
 
 export async function getServerSideProps() {
   // Fetch Data
@@ -11,16 +17,33 @@ export async function getServerSideProps() {
 
 const QRList = ({ data }) => {
   return (
-    <>
-      {data.map((qr, index) => {
-        return (
-          <div key={index}>
-            <QRCodeImage id={qr.id} />
-            <div>{qr.label}</div>
-          </div>
-        );
-      })}
-    </>
+    <Layout>
+      <Head>
+        <title>iSocks | Admin QR Management</title>
+      </Head>
+      <Container>
+        <Row>
+          <Col>
+          <Heading>QR Management</Heading>
+          <Heading fontWeight="300">Edit/Delete QR Codes</Heading></Col>
+        </Row>
+        <QRContainer>
+          {data.map((qr, index) => {
+            return (
+              <CodeBox key={index}>
+                <QRCodeImage id={qr.id} />
+                <CodeLabel>{qr.label}</CodeLabel>
+                <LinkText>URL: <LinkHref target="_blank" href={qr.url}>{qr.url}</LinkHref></LinkText>
+              </CodeBox>
+            );
+          })}
+        </QRContainer>
+
+        <ButtonContainer marginTop="5rem">
+          <Button onClick={() => Router.back()}>Back</Button>
+        </ButtonContainer>
+      </Container>
+    </Layout>
   );
 };
 
