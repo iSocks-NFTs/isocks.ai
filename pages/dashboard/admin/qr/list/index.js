d8f0b7586be0a69ad9769d817a35362354e7d7af
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-27 10:32:33 +0100

    QR List

--- pages/dashboard/admin/qr/list/index.js
+++ pages/dashboard/admin/qr/list/index.js
@@ -1,9 +1,27 @@
-const QRList = () =>{
-    return(
-        <>
-        
-        </>
-    )
+import QRCodeImage from "../../../../../components/QR/Module";
+
+export async function getServerSideProps() {
+  // Fetch Data
+  const response = await fetch("https://isocksnft.herokuapp.com/api/find/qr");
+  const data = await response.json();
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
 }
 
-export default QRList
+const QRList = ({ data }) => {
+  return (
+    <>
+      {data.map((qr, index) => {
+        return (
+          <div key={index}>
+            <QRCodeImage id={qr.id} />
+            <div>{qr.label}</div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default QRList;
