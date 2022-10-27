274fe0e3f4eed13f46e868dd1a735ddbad01dca2
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-27 10:41:19 +0100

    made update

--- components/QR/Module/index.js
+++ components/QR/Module/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Image } from "../style";
 import QRCode from "qrcode";
-import { GlobalContext } from "../../../context/globalContext";
 
 const QRCodeImage = ({ id }) => {
   const [src, setSrc] = React.useState();
-  const { baseUrl } = React.useContext(GlobalContext);
   const urlData = 'https://isocksv2.netlify.app' + '/qr/' + id
   console.log(urlData);
 
-  QRCode.toDataURL(id).then((data) => {
+  QRCode.toDataURL(urlData).then((data) => {
     setSrc(data);
   });
 
