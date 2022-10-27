6b4aacc12868e5905dcc9164525b47ab656a1da8
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-27 10:33:52 +0100

    made fix

--- pages/qr/[id].js
+++ pages/qr/[id].js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 // import QRCodeImage from "../../components/QR/Module";
 export async function getServerSideProps({ context }) {
-  const { id } = router.query;
+  const { id } = context.query;
   // Fetch Data
   const response = await fetch(`https://isocksnft.herokuapp.com/api/find/qr/${id}`);
   const data = await response.json();
