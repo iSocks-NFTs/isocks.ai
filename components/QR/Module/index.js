9066257419e3d5832b6972174b69c0ffa14f3ce9
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-27 10:14:10 +0100

    set up routing for qr code

--- components/QR/Module/index.js
+++ components/QR/Module/index.js
@@ -1,9 +1,14 @@
+import React from "react";
 import { Image } from "../style";
 import QRCode from "qrcode";
-import React from "react";
+import { GlobalContext } from "../../../context/globalContext";
 
 const QRCodeImage = ({ id }) => {
   const [src, setSrc] = React.useState();
+  const { baseUrl } = React.useContext(GlobalContext);
+  const urlData = 'https://isocksv2.netlify.app' + '/qr/' + id
+  console.log(urlData);
+
   QRCode.toDataURL(id).then((data) => {
     setSrc(data);
   });
