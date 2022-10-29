bbaf38062f149f346975ca1da67179fb35668dea
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-29 02:16:29 +0100

    added web3 context

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,24 @@ import React from "react";
 import GlobalContextProvider from "../context/globalContext";
 import AuthContextProvider from "../context/authContext";
 import TransactionContextProvider from "../context/transactionContext";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider, connector) {
+  return new Web3Provider(provider);
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalContextProvider>
-      <AuthContextProvider>
-        <TransactionContextProvider>
-          <Component {...pageProps} />
-        </TransactionContextProvider>
-      </AuthContextProvider>
-    </GlobalContextProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <GlobalContextProvider>
+        <AuthContextProvider>
+          <TransactionContextProvider>
+            <Component {...pageProps} />
+          </TransactionContextProvider>
+        </AuthContextProvider>
+      </GlobalContextProvider>
+    </Web3ReactProvider>
   );
 }
 
