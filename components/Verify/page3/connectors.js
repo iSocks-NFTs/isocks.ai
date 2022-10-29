8a78c685ea04f26aea2e847a34897a787a678e59
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-29 02:23:38 +0100

    completed updates

--- components/Verify/page3/connectors.js
+++ components/Verify/page3/connectors.js
@@ -6,14 +6,14 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 export const injected = new InjectedConnector({ supportedChainIds: [1] });
 
 export const walletconnect = new WalletConnectConnector({
-  infuraId: process.env.INFURA_ID,
+  infuraId: '24a33794805b4a2c962e674f31277ba4',
   qrcode: true,
 });
 
 export const uauth = new UAuthConnector({
-  clientID: process.env.clientID,
-  redirectUri: process.env.redirectUri,
-  postLogoutRedirectUri: process.env.redirectUri,
+  clientID: "8d942179-0841-496c-a1d4-a6c87b19636b",
+  redirectUri:"http://127.0.0.1:3000/verify",
+  postLogoutRedirectUri: "http://127.0.0.1:3000/verify",
   // Scope must include openid and wallet
   scope: "openid wallet",
 
--- components/Verify/page3/index.js
+++ components/Verify/page3/index.js
@@ -12,70 +12,44 @@ import { Button } from "../../Form";
 import { motion } from "framer-motion";
 import { UD } from "../../Form";
 import NonSSRWrapper from "../../no-ssr-wrapper";
-import * as UAuthWeb3Modal from "@uauth/web3modal";
-import UAuthSPA from "@uauth/js";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import Web3Modal from "web3modal";
+import { useWeb3React } from "@web3-react/core";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { AuthContext } from "../../../context/authContext";
+import { useSigner } from '@web3modal/react'
+import UAuth from "@uauth/js";
+import { UAuthConnector } from "@uauth/web3-react";import {InjectedConnector} from '@web3-react/injected-connector'
+import { useAccount } from "@web3modal/react";
 
-// These options are used to construct the UAuthSPA instance.
-const uauthOptions = {
-  clientID: "8d942179-0841-496c-a1d4-a6c87b19636b",
-  redirectUri: "https://isocksv2.netlify.app/verify",
-  scope: "openid wallet",
-};
+export const injected = new InjectedConnector({supportedChainIds: [1]})
 
-const providerOptions = {
-  // Currently the package isn't inside the web3modal library. For now,
-  // users must use this libary to create a custom web3modal provider.
+export const walletconnect = new WalletConnectConnector({
+  infuraId: "24a33794805b4a2c962e674f31277ba4",
+  qrcode: true,
+})
 
-  // All custom `web3modal` providers must be registered using the "custom-"
-  // prefix.
-  "custom-uauth": {
-    // The UI Assets
-    display: UAuthWeb3Modal.display,
-
-    // The Connector
-    connector: UAuthWeb3Modal.connector,
-
-    // The SPA libary
-    package: UAuthSPA,
-
-    // The SPA libary options
-    options: uauthOptions,
-  },
-
-  // For full functionality we include the walletconnect provider as well.
-  walletconnect: {
-    package: WalletConnectProvider,
-    options: {
-      infuraId: "24a33794805b4a2c962e674f31277ba4",
-    },
-  },
-
-  // Include any other web3modal providers here
-};
+const uauth = new UAuthConnector({
+  uauth: new UAuth({
+    clientID: "8d942179-0841-496c-a1d4-a6c87b19636b",
+    redirectUri: "https://isocksv2.netlify.app/verify",
+    postLogoutRedirectUri: "https://isocksv2.netlify.app/verify",
+    scope: "openid wallet",
+  }),
+  connectors: { injected, walletconnect },
+});
 
 const Step3 = ({ page, setPage, formData, setFormData }) => {
   const inputRef = React.useRef();
-  const { udUsername, udLogOut } = React.useContext(AuthContext);
   const [ud, setUd] = React.useState("");
-  const web3modal = new Web3Modal({ providerOptions });
-
-  // Register the web3modal so the connector has access to it.
-  UAuthWeb3Modal.registerWeb3Modal(web3modal);
+  const { activate,deactivate } = useWeb3React();
+  const { account } = useAccount()
 
   async function handleLogin() {
-    const provider = await web3modal.connect();
+    await activate(uauth);
   }
 
   async function handleLogout() {
     setUd("");
     window.localStorage.clear();
-    if (web3modal.cachedProvider === "custom-uauth") {
-      await uauth.logout();
-    }
-    web3modal.clearCachedProvider();
   }
 
   React.useEffect(() => {
@@ -84,10 +58,10 @@ const Step3 = ({ page, setPage, formData, setFormData }) => {
       let domain = JSON.parse(ud);
       setUd(domain.value);
     }
-  }, [udUsername]);
+  }, [account]);
 
   return (
-    <NonSSRWrapper>
+    <>
       <FormContainer
         marginTop="1rem"
         as={motion.div}
@@ -139,7 +113,7 @@ const Step3 = ({ page, setPage, formData, setFormData }) => {
           <Button type="button">Continue</Button>
         </Form>
       </FormContainer>
-    </NonSSRWrapper>
+    </>
   );
 };
 
