55cadd4c4a16f38fae96e34202b0a1e7edf59d8f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-31 02:47:54 +0100

    added navigation to go back

--- pages/dashboard/admin/account/[id].js
+++ pages/dashboard/admin/account/[id].js
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import Router from "next/router";
 import { Container, Heading } from "../../../../components/Dashboard/style";
 import AccountInfo from "../../../../components/Dashboard/Account";
 import Layout from "../../../../layouts/admin_layout";
+import { Button, ButtonContainer } from "../../../../components/Form";
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
@@ -16,14 +18,18 @@ export async function getServerSideProps(context) {
 }
 
 const Account = ({ data }) => {
+  const router = Router;
   return (
     <Layout>
       <Head>
         <title>iSocks | User Account Settings</title>
       </Head>
-      <Container height="100vh">
+      <Container height="fit-content">
         <Heading>User Account</Heading>
         <AccountInfo data={data} />
+        <ButtonContainer marginTop="2rem">
+          <Button onClick={() => router.back()}>Back</Button>
+        </ButtonContainer>
       </Container>
     </Layout>
   );
