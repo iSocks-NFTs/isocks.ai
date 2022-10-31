80d4b9aba645092af4cfeb320fdf7e71be7e769f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-10-31 06:24:38 +0100

    cleaned up code

--- pages/dashboard/admin/qr/index.js
+++ pages/dashboard/admin/qr/index.js
@@ -1,20 +1,13 @@
 import Head from "next/head";
 import React from "react";
 import Layout from "../../../../layouts/admin_layout";
-import { AuthContext } from "../../../../context/authContext";
 import Router from "next/router";
 import QRComponent from "../../../../components/QR";
-import { QRCode } from "qrcode";
+
 
 const QRManagement = () => {
   const router = Router;
 
-  // React.useEffect(() => {
-  //   if (!isLoggedIn) {
-  //     router.push("/dashboard/auth");
-  //   }
-  // }, [isLoggedIn, router]);
-  
   return (
     <Layout>
       <Head>
