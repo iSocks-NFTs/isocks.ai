a1f545577192910fdcb910a6f86c8bd75373f0ba
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-11-01 21:04:30 +0100

    finished qr scan redirection page

--- pages/qr/[id].js
+++ pages/qr/[id].js
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { TailSpin } from "react-loader-spinner";
+import Head from "next/head";
+
 // import QRCodeImage from "../../components/QR/Module";
 export async function getServerSideProps(context) {
   const { id } = context.query;
@@ -14,27 +19,57 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background-color: var(--primary-brand);
-  color:#fff;
+  color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size:32px;
-  height:100vh;
-  width:100%;
-`
+  font-size: 32px;
+  height: 100vh;
+  width: 100%;
+`;
 
 const TextBox = styled.div`
-  h3{
-    text-align: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  .label{
+    font-weight: bolder;
+    font-size:28px;
   }
-`
+  .url{
+    font-size: 20px;
+  }
+`;
 
 const QR = ({ data }) => {
+  const router = useRouter();
+  useEffect(() => {
+    if(data.url){
+      setTimeout(() => {
+        router.push(data.url);
+      }, 5000);
+    }
+  }, []);
+
   return (
     <Container>
+      <Head>
+        <title>iSocks QR | {data.label ? data.label : 'Invalid QR Code'}</title>
+      </Head>
       <TextBox>
-        <h3>{data?.label}</h3>
-        <Link href={data?.url}>Click Link</Link>
+        <TailSpin
+          height="25"
+          width="25"
+          color="#fff"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+        <span className="label">{data?.label}</span>
+        <span className="url">{data?.url}</span>
+        {data.url ? <Link href={data.url ? data.url : 'isocks.ai'}>Redirecting to link shortly</Link> : "Invalid QR Code"}
       </TextBox>
     </Container>
   );
