f96413d5825bd92529f1f5044649a952a28683e4
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-12-04 18:21:33 +0100

    added key

--- pages/api/axios.js
+++ pages/api/axios.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import { baseURL } from '../../config';
 export default axios.create({
-    baseURL
+    baseURL,
+    headers:{
+        x_api_key:process.env.NEXT_PUBLIC_BACKEND_KEY
+    },
+
 });
