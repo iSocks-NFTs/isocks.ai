f233c2b70cce763ebc3a0e7bd7686a75d7f3751b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-12-05 17:14:03 +0100

    removed unused variables

--- pages/dashboard/admin/qr/list/index.js
+++ pages/dashboard/admin/qr/list/index.js
@@ -7,7 +7,7 @@ const QRCodeImage = dynamic(
 );
 import Layout from "../../../../../layouts/admin_layout";
 import { Container, Heading } from "../../../../../components/Dashboard/style";
-import { Button, ButtonContainer, Label } from "../../../../../components/Form";
+import { Button, } from "../../../../../components/Form";
 import { useRouter } from "next/router";
 import { Row, Col } from "react-bootstrap";
 import {
