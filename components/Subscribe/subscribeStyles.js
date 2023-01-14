df9026a3822e66ce5229e6f68728821d34c21742
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-14 19:58:56 +0100

    increased height

--- components/Subscribe/subscribeStyles.js
+++ components/Subscribe/subscribeStyles.js
@@ -1,68 +1,71 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    max-width: 100%;
-    height:30vh;
-    position: relative;
-    padding-top:1rem;
-    background-color: #FAFAFA;
-    @media screen and (max-width:330px) {
-        padding-top:0;
-    }
-`
+  max-width: 100%;
+  height: 40vh;
+  position: relative;
+  padding-top: 1rem;
+  background-color: #fafafa;
+  @media screen and (max-width: 330px) {
+    padding-top: 0;
+  }
+`;
 
 export const Heading = styled.h3`
-    font-family: "Nunito Sans";
-    text-align:center;
-    font-weight: 600;
-    font-size:35px;
-    margin-bottom:-0.1rem;
-`
+  font-family: "Nunito Sans";
+  text-align: center;
+  font-weight: 600;
+  font-size: 35px;
+  margin-bottom: -0.1rem;
+`;
 
 export const Form = styled.form`
-    border:none;
-    height:100%;
-    width:100%;
-    display: flex;
-    align-items: center;
-`
+  border: none;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
 
 export const FormContainer = styled.div`
-    margin:1rem 0;
-    border:1px solid #E1E1E1;
-    border-radius: 32px;
-    height:50px;
-    margin:0 auto;
-    width:30%;
-    margin-top: 1rem;
-    background-color: #fff;
-    @media screen and (max-width:480px) {
-        width:80%;
-    }
-`
+  margin: 1rem 0;
+  border: 1px solid #e1e1e1;
+  border-radius: 32px;
+  height: 50px;
+  margin: 0 auto;
+  width: 30%;
+  margin-top: 1rem;
+  background-color: #fff;
+  @media screen and (max-width: 480px) {
+    width: 80%;
+  }
+  .status {
+    text-align: center;
+  }
+`;
 
 export const Input = styled.input`
-    height:100%;
-    border-radius: 32px;
-    width:100%;
-    background-color: #fff;
-    color:var(--primary-brand);
-    padding-left:10px;
-    font-size:13px;
-    border:none;
-    :focus{
-        outline:none;
-    }
-`
+  height: 100%;
+  border-radius: 32px;
+  width: 100%;
+  background-color: #fff;
+  color: var(--primary-brand);
+  padding-left: 10px;
+  font-size: 13px;
+  border: none;
+  :focus {
+    outline: none;
+  }
+`;
 
 export const Button = styled.button`
-    background-color: var(--primary-brand);
-    border-radius: 32px;
-    border:none;
-    padding:15px 20px;
-    border-color:#E1E1E1;
-    color:#fff;
-    :hover{
-        cursor:pointer;
-    }
-`
+  background-color: var(--primary-brand);
+  border-radius: 32px;
+  border: none;
+  padding: 15px 20px;
+  border-color: #e1e1e1;
+  color: #fff;
+  :hover {
+    cursor: pointer;
+  }
+`;
