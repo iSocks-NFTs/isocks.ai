4ce357e98be521cc16139026446dce0d75354e03
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-14 10:07:06 +0100

    updated email

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,46 +1,67 @@
 import React from "react";
-import {GlobalContext} from '../../context/globalContext'
-import { Container, Image,Grid, ImgCaption,Heading,ContactUs,SocialContainer,Socials,Information,Brand } from "./footerStyles";
-import Link from 'next/link';
+import { GlobalContext } from "../../context/globalContext";
+import {
+  Container,
+  Image,
+  Grid,
+  ImgCaption,
+  Heading,
+  ContactUs,
+  SocialContainer,
+  Socials,
+  Information,
+  Brand,
+} from "./footerStyles";
+import Link from "next/link";
 import { FaFacebookSquare } from "react-icons/fa";
 
-const Footer = () =>{
-    return(
-        <Container>
-            <Grid>
-                <Brand>
-                    <Link href="/"><Image src="/img/logo/logo_bg.png" alt="iSocks Logo" /></Link>
-                    <ImgCaption>A product of infinity Auctions</ImgCaption>
-                </Brand>
-                <Information>
-                    <Socials>
-                        <Heading>Socials</Heading>
-                        <SocialContainer>
-                            <Link href="https://www.facebook.com/100084982976478/posts/pfbid02n18vDRwtfzP8xeg1cuQeV4PHJHHZguSXydhU3vw1yzCS2x4dFC5ENo26eZB4qt4Ql/" target="_blank">
-                                <FaFacebookSquare size={25} color="#fff" />
-                            </Link>
-                            <Link href="https://www.tiktok.com/@isocksnft?_t=8VmXPtv8jH4&_r=1" target="_blank">
-                                <Image src="/img/logo/tiktok.svg" alt="icon"/>
-                            </Link>
-                            <Link href="https://mobile.twitter.com/isocksNft" target="_blank">
-                                <Image src="/img/logo/twitter.svg" alt="icon"/>
-                            </Link>
-                            <Link href="https://www.instagram.com/isocksnft/" target="_blank">
-                                <Image src="/img/logo/ig.svg" alt="icon"/>
-                            </Link>
-                            <Link href="https://discord.gg/nbrsZY9z59" target="_blank">
-                                <Image src="/img/logo/discord.svg" alt="icon"/>
-                            </Link>
-                        </SocialContainer>
-                    </Socials>
-                    <ContactUs>
-                        <span>Contact Us</span>
-                        <Link className="email" href="mailto:infinityauctionnft@gmail.com">infinityauctionnft@gmail.com</Link>
-                    </ContactUs>
-                </Information>
-            </Grid>
-        </Container>
-    )
-}
+const Footer = () => {
+  return (
+    <Container>
+      <Grid>
+        <Brand>
+          <Link href="/">
+            <Image src="/img/logo/logo_bg.png" alt="iSocks Logo" />
+          </Link>
+          <ImgCaption>A product of infinity Auctions</ImgCaption>
+        </Brand>
+        <Information>
+          <Socials>
+            <Heading>Socials</Heading>
+            <SocialContainer>
+              <Link
+                href="https://www.facebook.com/100084982976478/posts/pfbid02n18vDRwtfzP8xeg1cuQeV4PHJHHZguSXydhU3vw1yzCS2x4dFC5ENo26eZB4qt4Ql/"
+                target="_blank"
+              >
+                <FaFacebookSquare size={25} color="#fff" />
+              </Link>
+              <Link
+                href="https://www.tiktok.com/@isocksnft?_t=8VmXPtv8jH4&_r=1"
+                target="_blank"
+              >
+                <Image src="/img/logo/tiktok.svg" alt="icon" />
+              </Link>
+              <Link href="https://mobile.twitter.com/isocksNft" target="_blank">
+                <Image src="/img/logo/twitter.svg" alt="icon" />
+              </Link>
+              <Link href="https://www.instagram.com/isocksnft/" target="_blank">
+                <Image src="/img/logo/ig.svg" alt="icon" />
+              </Link>
+              <Link href="https://discord.gg/nbrsZY9z59" target="_blank">
+                <Image src="/img/logo/discord.svg" alt="icon" />
+              </Link>
+            </SocialContainer>
+          </Socials>
+          <ContactUs>
+            <span>Contact Us</span>
+            <Link className="email" href="mailto:isocksnftofficial@gmail.com">
+              isocksnftofficial@gmail.com
+            </Link>
+          </ContactUs>
+        </Information>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Footer;
