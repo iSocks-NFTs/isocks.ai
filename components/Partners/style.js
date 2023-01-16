42195dcb3d38771b69409a9f4ccd4e0778c2d364
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-16 15:49:46 +0100

    styled components for partner page

---
+++ components/Partners/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: fit-content;
+  margin: 0 auto;
+  background-color: #fff;
+  padding: 1.5rem;
+  border-radius:8px;
+  box-shadow:5px 5px #f5f5f5;
+`;
