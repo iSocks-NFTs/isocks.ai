d7a049c1ea8dd2aaa6d53c945a8af6f7b23051b5
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-18 03:00:43 +0100

    added fail modal

--- components/Partners/index.js
+++ components/Partners/index.js
@@ -56,6 +56,7 @@ export default function Page() {
       })
       .catch((err) => {
         setLoading(false);
+        setModal({ ...modal, failModal: true });
         clearFields();
         console.log(err);
       });
