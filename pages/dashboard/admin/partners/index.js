606e68110693194b4bbf1d1d8148679f7746d62d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-20 00:47:12 +0100

    list of partners for dashboard

--- pages/dashboard/admin/partners/index.js
+++ pages/dashboard/admin/partners/index.js
@@ -1,17 +1,35 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
-import styled from "styled-components";
-import Layout from "../../../../layouts/admin_layout";
+import { TailSpin } from "react-loader-spinner";
 import {
   Container,
   Heading,
   Text,
 } from "../../../../components/Dashboard/style";
-import { ButtonContainer, Button } from "../../../../components/Form";
-import { TailSpin } from "react-loader-spinner";
-import { useRouter } from "next/router";
+import { Button, ButtonContainer } from "../../../../components/Form";
+import { baseURL } from "../../../../config";
+import Layout from "../../../../layouts/admin_layout";
 
-export default function Partners({}) {
+export async function getServerSideProps() {
+  const GET_PARTNERS = "/api/partners";
+  const response = await fetch(`${baseURL + GET_PARTNERS}`, {
+    headers: {
+      key: process.env.NEXT_PUBLIC_BACKEND_KEY,
+    },
+  });
+
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Partners({ data }) {
+  console.log(data);
   const [goBack, setBack] = React.useState(false);
   const router = useRouter();
 
@@ -28,8 +46,9 @@ export default function Partners({}) {
       <Container>
         <Heading>Review Partners</Heading>
         <Text textAlign="center" fontSize="18px">
-          Look through list of partners
+          Look through list of applied partners
         </Text>
+        
         <ButtonContainer>
           <Button onClick={back}>
             {goBack ? (
