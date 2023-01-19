c2c465b29fff54463d64353908deebc9b6095443
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-20 00:46:19 +0100

    passed vendors received from database

--- pages/buy/index.js
+++ pages/buy/index.js
@@ -2,15 +2,33 @@ import Head from "next/head";
 import BuyOptionsComponent from "../../components/BuyOption";
 import Layout from "../../layouts/normal_layout";
 
-const BuyOption = () => {
+import { baseURL } from "../../config";
+
+export async function getServerSideProps() {
+  const endpoint = "/api/user/vendor";
+
+  const response = await fetch(`${baseURL + endpoint}`, {
+    headers: {
+      key: process.env.NEXT_PUBLIC_BACKEND_KEY,
+    },
+  });
+
+  const data = await response.json();
+
+  return {
+    props: { data },
+  };
+}
+
+const BuyOption = ({ data }) => {
   return (
     <Layout>
-        <Head>
-            <title>iSocks | Choose Buying Option</title>
-        </Head>
-        <BuyOptionsComponent />
+      <Head>
+        <title>iSocks | Choose Buying Option</title>
+      </Head>
+      <BuyOptionsComponent vendors={data} />
     </Layout>
-  )
+  );
 };
 
 export default BuyOption;
