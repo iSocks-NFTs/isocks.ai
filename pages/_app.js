67ee7f9709e936ae9cfc8642ba73311cd3044489
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-24 07:55:27 +0100

    set up authentication

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,21 @@ import AuthContextProvider from "../context/authContext";
 import TransactionContextProvider from "../context/transactionContext";
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
+import { createClient, configureChains, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { SessionProvider } from "next-auth/react";
+import { mainnet } from "wagmi/chains";
+
+const { provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+});
 
 function getLibrary(provider, connector) {
   return new Web3Provider(provider);
@@ -13,13 +28,17 @@ function getLibrary(provider, connector) {
 function MyApp({ Component, pageProps }) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <GlobalContextProvider>
-        <AuthContextProvider>
-          <TransactionContextProvider>
-            <Component {...pageProps} />
-          </TransactionContextProvider>
-        </AuthContextProvider>
-      </GlobalContextProvider>
+      <WagmiConfig client={client}>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <GlobalContextProvider>
+            <AuthContextProvider>
+              <TransactionContextProvider>
+                <Component {...pageProps} />
+              </TransactionContextProvider>
+            </AuthContextProvider>
+          </GlobalContextProvider>
+        </SessionProvider>
+      </WagmiConfig>
     </Web3ReactProvider>
   );
 }
