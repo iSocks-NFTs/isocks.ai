d916836c38b5a6838d98b838efb7bf0f81ba4697
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-26 14:14:01 +0100

    updated text

--- components/ChatSection/index.js
+++ components/ChatSection/index.js
@@ -8,18 +8,18 @@ export default function ChatSection() {
   return (
     <Container>
       <div class="text">
-        <Heading>iSocks Live Chat</Heading>
+        <Heading>iSocks Wallet-to-Wallet Chat</Heading>
         <TextBody>
-          Join our group chat and connect with like-minded individuals who share
-          your passion for fashion and technology. Share your thoughts, ideas
-          and advice, and be a part of our community. We strive to provide a
-          space where you can feel comfortable, and where you can be heard.
+          We are excited to offer our customers a convenient and secure
+          wallet-to-wallet live chat service that is available 24/7. This
+          feature allows you to chat with other NFT holders in real-time.
+          However please be aware that this service is in Beta V1 Phase and will only be available for iSocks holders after our launch.
+          But we are giving you the opportunity to enjoy it free now and experience of Web3 before the launch.
           <br />
           <br />
-          Don't miss out on the action! Click the "Join The Community" button to
-          join our group chat now and start connecting with our community."
+          Don't miss out on the action! Click the "Connect Wallet" button now and start connecting with our community."
         </TextBody>
-        <Button onClick={() => push("/chat")}>Join The Community</Button>
+        <Button onClick={() => push("/chat")}>Connect Wallet</Button>
       </div>
       <Image src={`/img/chat/chat02.jpg`} width="450" height="450px" />
     </Container>
