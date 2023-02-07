fa6ddf6ddbdb6d257f8a009b0da2fc4852f73b34
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-02-07 23:40:48 +0100

    set up static import

--- components/Products/ProductSection/index.js
+++ components/Products/ProductSection/index.js
@@ -2,19 +2,21 @@ import { Container, Heading, TextBody } from "./style";
 import { Button } from "../../Form";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import socks from "../../../public/img/products/sock_wrapped.jpg";
+import thePackage from "../../../public/img/products/package.jpg";
 
 export function ProductSection() {
   const { push } = useRouter();
 
   const products = [
     {
-      imgUrl: "/img/products/sock_wrapped.jpg",
+      imgUrl: socks,
       heading: "The iSocks",
       description:
         "The ultimate in luxury and craftsmanship. Made from 100% handpicked fine cotton, our socks are handmade from start to finish, ensuring the highest quality and attention to detail.",
     },
     {
-      imgUrl: "/img/products/package.jpg",
+      imgUrl: thePackage,
       heading: "The Package",
       description:
         "A work of art in itself. Designed to perfectly complement our handmade, premium socks, each box is made from high-quality materials and features unique, eye catching designs, Whether it's for personal use or as a gift, the iSocksNFT packaging box adds an extra touch of luxury to the already premium product.",
@@ -34,7 +36,7 @@ export function ProductSection() {
               className="text-center sm:w-[350px] sm:h-[480px] w-[280px]  flex flex-col justify-start border-2 rounded-md  hover:scale-105 duration-500 ease-in hover:cursor-pointer"
               key={index}
             >
-              <img
+              <Image
                 src={product.imgUrl}
                 className="rounded-md object-contain"
                 alt={product.heading}
