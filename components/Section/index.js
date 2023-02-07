1fedfa334f2537afe4da696131fda09efa9da127
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-02-07 07:43:34 +0100

    section update

--- components/Section/index.js
+++ components/Section/index.js
@@ -3,12 +3,17 @@ import { Heading, P } from "../Roadmap/RoadmapStyles";
 
 export default function Section() {
   return (
-    <Container>
-      <Heading>Our Vision</Heading>
-      <P>Long-term goals and objectives</P>
-      <div className="section">
-        <Image src="/img/section/wb.png" alt="White and Black Photo" />
-        <p className="quote">
+    <div className="w-full flex sm:flex-row md:flex-col lg:flex-row flex-col h-fit">
+      <div className="sm:w-[60%] md:w-full lg:w-[55%] w-full">
+        <img src="/img/section/wb.png" alt="White and Black Photo" />
+      </div>
+      <div className="sm:w-[40%] md:w-full lg:w-[45%] w-full bg-black text-white sm:text-right md:text-center lg:text-right text-center flex flex-col justify-center p-2">
+        <h3 className="sm:text-[3.2rem] text-[2.8rem]">Our Vision</h3>
+        <p className="sm:text-[2.5rem] text-[2rem] font-semibold">
+          Long-term goals and objectives
+        </p>
+        <p className="sm:text-[1.7rem] text-[1rem] font-thin">
+          {" "}
           "We believe that the future of fashion is about more than just looking
           good, it is about being able to prove the authencity and ownership of
           your wardrobe. Join us on our journey to revolutionize the way we
@@ -16,6 +21,6 @@ export default function Section() {
           your style with iSocksNFT."
         </p>
       </div>
-    </Container>
+    </div>
   );
 }
