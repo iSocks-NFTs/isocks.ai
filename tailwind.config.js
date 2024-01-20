5445d18659ede7537d60e1d38e9c6cd436c21fcb
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2024-01-20 10:05:36 +0100

    feat(style): set up new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Zen-Dots": ["Zen Dots", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
